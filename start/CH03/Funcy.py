759e0c379e0f41d150febd4db9a9cadc50f1eb8e
Mark <Vasylchuk.Mark@student.greenriver.edu>
2024-07-16 23:18:25 -0700

    changes 2.0

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -2,6 +2,7 @@
 # example workign with Functions
 userInput = input("Is today a good day? (y/n) ")
 if userInput == "y":
-    print("Yes it is")
+    for i in range(10):
+        print("Yes it is")
 else:
     print("No its not")
