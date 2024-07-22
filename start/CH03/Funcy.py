6e6133986ce395df958c6447314fb08643674fb8
Mark <Vasylchuk.Mark@student.greenriver.edu>
2024-07-22 13:18:50 -0700

    changes 2

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # example workign with Functions
-userInput = input("Is today a good day? (y/n) ")
-if userInput == "y":
-    for i in range(10):
-        print("Yes it is")
-else:
-    print("No its not")
+def send_message():
+    userInput = input("Is today a good day? (y/n) ")
+    if userInput == "y":
+        for i in range(10):
+            print("Yes it is")
+    else:
+        print("No its not")
+send_message()
