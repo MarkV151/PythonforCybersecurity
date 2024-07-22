89815c32b260a2b849ef3accb47ca543547a3667
Mark <Vasylchuk.Mark@student.greenriver.edu>
2024-07-22 13:09:46 -0700

    yup

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+test = open("hackme.txt", "w")
+
+name = input("What is your name: ")
+color = input("What is your favorite color: ")
+pet = input("What was your first pet's name: ")
+mom = input("What is your mother's maiden name: ")
+school = input("What elementary school did you attend: ")
+
+test.write(name)
+test.write(color)
+test.write(pet)
+test.write(mom)
+test.write(school)
