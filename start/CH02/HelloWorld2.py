6b4c2dbc72b8724aa37f92e6760e8258c10ccd47
Mark <Vasylchuk.Mark@student.greenriver.edu>
2024-08-02 00:09:48 -0700

    yup

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
 # Created 
+name = input("What is your name: ")
+print("Hello", name)
+print("Today will be a great day")
 
-# Suggestion, build out 1 line at a time
-# Once multiple print statemetns exist, put a breakpoint at first print line
-# Then walk through as an example of "debugging"
+age = input("What is your age: ")
+age = int(age)
+age2 = age + 2
+print("In 2 years, you will be", age2, "years old")
--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,20 @@
-#!/usr/bin/env python3
-# Script that encrypts/decrypts text using ROT13
-# By 
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+def main():
+    try:
+        num = int(input("Enter a number: "))
+        if is_prime(num):
+            print(f"{num} is a prime number.")
+        else:
+            print(f"{num} is not a prime number.")
+    except ValueError:
+        print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    main()
